0d1342535f45ffb8e6b13086c83232114495b60d
AleksanderMorgała <jaolekm@gmail.com>
2020-12-10 16:47:45 +0100

    Day 2 rework - slightly changed

--- Day10/main.py
+++ Day10/main.py
@@ -15,6 +15,9 @@ print (counter[0]*counter[2])
 
 
 
+
+#part2
+
 def find_a_ways(data,x=0):
 	for index in range(len(data)-1):
 		i = index + 1
@@ -32,6 +35,6 @@ def find_a_ways(data,x=0):
 data.insert(0,0)
 data = [[x,0] for x in data]
 data[0][1] = 1 
-print (find_a_ways(data))
+print (find_a_ways(data)[len(data)-2][1])
 
 
--- Day2/main.py
+++ Day2/main.py
@@ -2,7 +2,7 @@ import re
 
 def check_password(password_line):
 	regex_filter = "([0-9]+)-([0-9]+) (.): (.*)"
-	x = re.split(regex_filter,str(password_line))
+	x = re.match(regex_filter,str(password_line))
 	bot = int(x[1])
 	top = int(x[2])
 	letter = x[3]
@@ -14,25 +14,20 @@ def check_password(password_line):
 
 def new_check_password(password_line):
 	regex_filter = "([0-9]+)-([0-9]+) (.): (.*)"
-	x = re.split(regex_filter,str(password_line))
+	x = re.match(regex_filter,str(password_line))
 	bot = int(x[1])
 	top = int(x[2])
 	letter = x[3]
 	password = x[4]
-	print(bot,top,letter,password,count)
 	if (password[bot-1] == letter) ^ (password[top-1] == letter): 
 		return 1
 	return 0
 
 
 if __name__ == "__main__":
-	with open("input.txt") as input:
-		count = 0
-		count2=0
-		for password_line in input:
-			if check_password(password_line):
-				count+=1
-			if new_check_password(password_line):
-				count2+=1
-		print(count)
-		print(count2)
+	with open("input.txt") as input_data:
+		data = input_data.read().splitlines()
+		checked_passwords_part1 = sum(list(map(check_password,data)))
+		checked_passwords_part2 = sum(list(map(new_check_password,data)))
+		print(checked_passwords_part1)
+		print(checked_passwords_part2)
