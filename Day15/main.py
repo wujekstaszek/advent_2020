3d68d908c27ff93a56e53348ace33a7cec3496f2
AleksanderMorgała <jaolekm@gmail.com>
2020-12-15 07:09:18 +0100

    Day 15

--- Day15/main.py
+++ Day15/main.py
@@ -1,17 +1,11 @@
-
-
-
 data = [0,14,6,20,1,4,0]
 data_dic = {0:1,14:2,6:3,20:4,1:5,4:6}
-
-
-
-for turn in range(1,30000010):
+for turn in range(len(data),30000000):
 	num = data[-1]
 	if num in data_dic:
-		data.append(turn + 6 - data_dic[num])
-		data_dic[num] = turn + 6
+		data.append(turn - data_dic[num])
+		data_dic[num] = turn
 	else:
 		data.append(0)
-		data_dic[num] = turn + 6 
+		data_dic[num] = turn
 print(data[30000000-1])
